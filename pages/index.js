4bcd6a4d32b35fd62b6bcfb5e4650447e5bfe6a4
Syafiq-ma <alaudinsyafiq910@gmail.com>
2021-02-22 21:25:20 +0700

    edit index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss';
+import Link from 'next/link';
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Hallo</h1>
-      <h2>konect</h2>
+    <div>
+      <h1>konect</h1>
+      <Link href='/profile'>
+        <a>
+          <button>go to demo</button>
+        </a>
+      </Link>
     </div>
   )
 }
